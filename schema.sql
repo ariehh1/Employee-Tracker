0ed633bfcfcf644a3c49781572fc94a9f999c911
Ari Horowitz <ariehh@comcast.net>
2020-02-17 17:13:21 -0800

    adding seed.sql file and updating schema.sql with salary decimal specifications

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT FK_DepartmentRole FOREIGN KEY (department_id),
@@ -29,4 +29,10 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT NULL,
     PRIMARY KEY(id),
+    CONSTRAINT FK_RoleEmployee FOREIGN KEY (role_id)
+	    REFERENCES role(id)
+	    ON DELETE CASCADE
+	    ON UPDATE CASCADE,
+    CONSTRAINT FK_ManagerEmployee FOREIGN KEY (manager_id)
+	    REFERENCES employee(id)
 );
