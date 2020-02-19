2204e71b65040c73cd762942c143b40525879e02
Ari Horowitz <ariehh@comcast.net>
2020-02-18 20:01:41 -0800

    added package.json file

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const CFonts = require("cfonts");
+
+CFonts.say("Employee|Manager", {
+  font: "block", // define the font face
+  align: "left", // define text alignment
+  colors: ["system"], // define all colors
+  background: "transparent", // define the background color, you can also use `backgroundColor` here as key
+  letterSpacing: 1, // define letter spacing
+  lineHeight: 1, // define the line height
+  space: true, // define if the output text should have empty lines on top and on the bottom
+  maxLength: "0", // define how many character can be on one line
+  gradient: false, // define your two gradient colors
+  independentGradient: false // define if you want to recalculate the gradient for each new line
+});
 
 const promptMessages = {
   songsByArtist: "Find songs by artist",
