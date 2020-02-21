f9216203a56749c0cfdf4910710084c8598bcd2c
Ari Horowitz <ariehh@comcast.net>
2020-02-20 18:22:12 -0800

    restructuring seed.sql with departments and index.js file has user prompts

--- index.js
+++ index.js
@@ -60,10 +60,6 @@ function promptUser() {
           "Remove Employee",
           "Update Employee Role",
           "Update Employee Manager",
-          "View All Roles",
-          "Add Role",
-          "Remove Role",
-          "View All Departments",
           "Exit"
         ]
       }
--- seed.sql
+++ seed.sql
@@ -1,12 +1,28 @@
 INSERT INTO department (name) 
-VALUES ('Accounting'), ('HR'), ('Sales'), ('Finance'), ('Legal')
+VALUES ('Accounting'),
+
+INSERT INTO department (name)
+VALUES ('HR'),
+
+INSERT INTO department (name)
+VALUES ('Sales'),
+
+INSERT INTO department (name)
+VALUES ('Engineering'),
+
+INSERT INTO department (name)
+VALUES ('Finance'),
+
+INSERT INTO department (name) 
+VALUES ('Legal')
+
 -- look into syntax for mulitple inserts
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Manager', 150000, 1), ('Engineer', 90000, 2), ('Accountant', 97000, 1)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ari', 'Horowitz', 1, 1), ('Kobe', 'Bryant', 1, )
+VALUES ('Ari', 'Horowitz', 1, 1), ('Kobe', 'Bryant', 1, 1),
 
 
 -- developing id's with join
