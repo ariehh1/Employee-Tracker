9c685067a7e82915d359d98a84674512f8efc8ad
Ari Horowitz <ariehh@comcast.net>
2020-02-20 16:13:50 -0800

    changed salary decimal to have no remaining digits

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NULL,
-    salary DECIMAL (10,2) NOT NULL,
+    salary DECIMAL (10) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT FK_DepartmentRole FOREIGN KEY (department_id),
