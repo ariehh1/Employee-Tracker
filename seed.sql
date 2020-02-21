8854695eedbacbf5e48cbf0e117ec3a5c38680d0
Ari Horowitz <ariehh@comcast.net>
2020-02-20 18:00:48 -0800

    adding more department names

--- seed.sql
+++ seed.sql
@@ -1,12 +1,12 @@
 INSERT INTO department (name) 
-VALUES ('Accounting'), ('HR')
+VALUES ('Accounting'), ('HR'), ('Sales'), ('Finance'), ('Legal')
 -- look into syntax for mulitple inserts
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 150000, 1), ('Engineer', 78000, 2), ('Developer', 98000, 1)
+VALUES ('Manager', 150000, 1), ('Engineer', 90000, 2), ('Accountant', 97000, 1)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ari', 'Horowitz', 1, 1);
+VALUES ('Ari', 'Horowitz', 1, 1), ('Kobe', 'Bryant', 1, )
 
 
 -- developing id's with join
