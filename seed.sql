18c70a6eccd405f3bb8e211589d2c717fc056916
Ari Horowitz <ariehh@comcast.net>
2020-02-21 21:52:43 -0800

    prepopulated seed.sql data

--- seed.sql
+++ seed.sql
@@ -1,17 +1,29 @@
+USE employee_trackerDB;
+
 INSERT INTO department (name) 
-VALUES 
+VALUES
+('Sales'),
 ('Accounting'),
 ('HR'),
-('Sales'),
 ('Engineering'),
-('Finance'),
+('Legal'),
 ('Marketing');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Accountant', 97000, 1), ('Recruiter', 60000, 2), ('Account Executive', 85000, 3)
+VALUES 
+('Account Executive', 85000, 1)
+('Sales Manager', 91000, 1)
+('Accountant', 97000, 2)
+('Recruiter', 64000, 3), 
+('Full Stack Developer', 120000, 4),
+('Lawyer', 150000, 5),
+('Marketing Coordinator', 60000, 6),
+('Benefits Administrator', 77000, 3),
+('Marketing Manager', 80000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ari', 'Horowitz', 1, 1), ('Kobe', 'Bryant', 1, 1),
-
-
--- developing id's with join
+VALUES 
+('Ari', 'Horowitz', 2, null),
+('Kobe', 'Bryant', 1, 2),
+('Tiger', 'Woods', 3, 2),
+('Serena', 'Williams', 5, null),
