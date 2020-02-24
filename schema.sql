bd0833937c707f30bb05f70f587cbd2c924ce1d7
Ari Horowitz <ariehh@comcast.net>
2020-02-23 19:33:57 -0800

    final updates to schema.sql

--- schema.sql
+++ schema.sql
@@ -2,28 +2,28 @@ DROP DATABASE IF EXISTS employee_managerDB;
 
 CREATE DATABASE employee_managerDB;
 
-USE  employee_managerDB
+USE  employee_managerDB;
 
 CREATE TABLE employee (
      id INTEGER AUTO_INCREMENT NOT NULL,
      first_name VARCHAR (30)  NOT NULL,
      last_name VARCHAR (30) NOT NULL,
-     role_id  INTEGER REFERENCES role(id),
+     role_id INTEGER REFERENCES role(id),
      manager_id INTEGER REFERENCES department(id),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
      id INTEGER AUTO_INCREMENT NOT NULL,
-     department VARCHAR (50) NOT NULL,
-     manager VARCHAR (50) NOT NULL,
+     department VARCHAR (30) NOT NULL,
+     manager VARCHAR (30) NOT NULL,
      PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT NOT NULL,
-    title VARCHAR (50) NOT NULL,
-    salary DECIMAL(6) NOT NULL,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(10) NOT NULL,
     department_id INTEGER REFERENCES department(id),
     PRIMARY KEY (id)
 );
